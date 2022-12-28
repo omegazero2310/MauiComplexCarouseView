e8a0a64e380c10145e00a77b259d76eef1901ac1
Nguyễn Văn An <annv3@bagroup.vn>
2022-12-28 17:40:00 +0700

    add normal CarouselView

--- mauitest/MainPageViewModel.cs
+++ mauitest/MainPageViewModel.cs
@@ -11,8 +11,16 @@ namespace mauitest
             get { return _favouriteMenuItems; }
             set { SetProperty(ref _favouriteMenuItems, value); }
         }
+
+        private ObservableCollection<UserMenu> _normalMenuItems = new ObservableCollection<UserMenu>();
+        public ObservableCollection<UserMenu> MormalMenuItems
+        {
+            get { return _normalMenuItems; }
+            set { SetProperty(ref _normalMenuItems, value); }
+        }
         public MainPageViewModel()
 		{
+            #region Complex object
             FavouriteMenuItems.Add(new ItemSupport()
             {
                 FeaturesItem = new List<UserMenu>()
@@ -56,9 +64,40 @@ namespace mauitest
                     }
                 }
             });
+            #endregion
 
+            MormalMenuItems.Add(new UserMenu()
+            {
+                MenuName = "111",
+                ImageName = "dotnet_bot.png"
+            });
+            MormalMenuItems.Add(new UserMenu()
+            {
+                MenuName = "112",
+                ImageName = "dotnet_bot.png"
+            });
+            MormalMenuItems.Add(new UserMenu()
+            {
+                MenuName = "113",
+                ImageName = "dotnet_bot.png"
+            });
+            MormalMenuItems.Add(new UserMenu()
+            {
+                MenuName = "114",
+                ImageName = "dotnet_bot.png"
+            });
+            MormalMenuItems.Add(new UserMenu()
+            {
+                MenuName = "115",
+                ImageName = "dotnet_bot.png"
+            });
+            MormalMenuItems.Add(new UserMenu()
+            {
+                MenuName = "116",
+                ImageName = "dotnet_bot.png"
+            });
         }
-	}
+    }
     public class ItemSupport
     {
         public List<UserMenu> FeaturesItem { get; set; }
